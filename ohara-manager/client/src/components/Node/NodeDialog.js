6dbdff8dc11af29b995ad6b539f1aeeb8cc97894
Joshua <joshua@is-land.com.tw>
2020-03-17 13:55:50 +0800

    #4313 [Manager] Unavailable nodes should NOT be selectable (#4316)

--- ohara-manager/client/src/components/Node/NodeDialog.js
+++ ohara-manager/client/src/components/Node/NodeDialog.js
@@ -34,6 +34,7 @@ import {
 } from 'context';
 import { QuickSearch } from 'components/common/Search';
 import { MODE } from 'const';
+import { state } from '../../api/nodeApi';
 
 const Actions = styled.div`
   display: flex;
@@ -172,9 +173,13 @@ const NodeDialog = () => {
         };
       });
 
-  const blockedRows = rows.filter(row => {
-    return blockedNodes.map(node => node.hostname).includes(row.name);
-  });
+  const blockedRows = rows.filter(row =>
+    blockedNodes.map(node => node.hostname).includes(row.name),
+  );
+
+  const unavailableRows = rows.filter(
+    row => row.state.toUpperCase() === state.unavailable,
+  );
 
   return (
     <FullScreenDialog
@@ -217,7 +222,7 @@ const NodeDialog = () => {
           selected={selected}
           setSelected={rows => setData({ ...data, selected: rows })}
           blockedRows={blockedRows}
-          unavailableRows={rows.filter(row => row.state === 'UNAVAILABLE')}
+          unavailableRows={unavailableRows}
         />
 
         <AddNodeDialog
