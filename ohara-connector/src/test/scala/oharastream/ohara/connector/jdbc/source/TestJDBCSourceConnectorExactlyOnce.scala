d63856b2b476af97e6c3a03c454499ceda3d8e69
jackyoh <jack@is-land.com.tw>
2020-10-06 11:26:59 +0800

    #5434 Modify unit test for the TestJDBCSourceConnectorExactlyOnce (#5652)

--- ohara-connector/src/test/scala/oharastream/ohara/connector/jdbc/source/TestJDBCSourceConnectorExactlyOnce.scala
+++ ohara-connector/src/test/scala/oharastream/ohara/connector/jdbc/source/TestJDBCSourceConnectorExactlyOnce.scala
@@ -214,30 +214,28 @@ class TestJDBCSourceConnectorExactlyOnce extends With3Brokers3Workers {
     val statement = client.connection.createStatement()
     try {
       val resultSet = statement.executeQuery(s"select * from $tableName order by $queryColumnName")
-      val queryResult: (String, String) = Iterator
+      val queryResult: (Int, String, String) = Iterator
         .continually(resultSet)
         .takeWhile(_.next())
         .map { x =>
-          (x.getString(timestampColumnName), x.getString(queryColumnName))
+          (x.getInt(incrementColumnName), x.getString(timestampColumnName), x.getString(queryColumnName))
         }
         .toSeq
         .head
       awaitInsertDataCompleted(startTestTimestamp) // Wait data write to the table
-      statement.executeUpdate(s"DELETE FROM $tableName WHERE $queryColumnName='${queryResult._2}'")
+      statement.executeUpdate(s"DELETE FROM $tableName WHERE $incrementColumnName='${queryResult._1}'")
       statement.executeUpdate(
-        s"INSERT INTO $tableName($timestampColumnName, $queryColumnName) VALUES(NOW(), '${queryResult._2}')"
+        s"INSERT INTO $tableName($incrementColumnName, $timestampColumnName, $queryColumnName) VALUES(${queryResult._1}, '${queryResult._2}', '${queryResult._3}')"
       )
-      val result = consumer.poll(java.time.Duration.ofSeconds(30), tableTotalCount.intValue() + 1).asScala
-      tableTotalCount.intValue() + 1 shouldBe result.size
+      val result = consumer.poll(java.time.Duration.ofSeconds(30), tableTotalCount.intValue()).asScala
+      tableTotalCount.intValue() shouldBe result.size
       val topicData: Seq[String] = result
         .map(record => record.key.get.cell(queryColumnName).value().toString)
         .sorted[String]
         .toSeq
       val updateResultSet = statement.executeQuery(s"select * from $tableName order by $queryColumnName")
       val resultTableData: Seq[String] =
-        (Iterator.continually(updateResultSet).takeWhile(_.next()).map(_.getString(queryColumnName)).toSeq ++ Seq(
-          queryResult._2
-        )).sorted[String]
+        Iterator.continually(updateResultSet).takeWhile(_.next()).map(_.getString(queryColumnName)).toSeq
       checkData(resultTableData, topicData)
     } finally {
       result(connectorAdmin.delete(connectorKey)) // Avoid table not forund from the JDBC source connector
